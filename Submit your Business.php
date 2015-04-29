d0b558b59aa1fd5928b2d044dd88ea191de4e6dc
Dansmithyy <dan@todayintech.co.uk>
2015-04-29 19:53:43 +0100

    Built foundations to submit form

--- Submit your Business.php
+++
@@ -1,5 +0,0 @@
----
-layout: page
-title: Submit your Business to Gluten Free Exeter! 
-permalink: /submit/
----
---
+++ submitarea/Submit your Business.php
@@ -0,0 +1,34 @@
+---
+layout: page
+title: Submit your Business to Gluten Free Exeter! 
+permalink: /submit/
+---
+<form method="post">
+<div class="input-group" id="businessname">
+  <span class="input-group-addon" id="sizing-addon2">Business Name</span>
+  <input type="text" class="form-control" name="name" aria-describedby="sizing-addon2">
+</div>
+
+<br>
+
+<div class="input-group" id="businessaddress">
+  <span class="input-group-addon" id="sizing-addon2">Business Address</span>
+  <input type="text" class="form-control" name="address" aria-describedby="sizing-addon2">
+</div>
+
+<br>
+
+<div class="input-group" id="businessemail">
+  <span class="input-group-addon" id="sizing-addon2">Email Address</span>
+  <input type="email" class="form-control" name="email" aria-describedby="sizing-addon2">
+</div>
+
+<br>
+
+<div class="panel panel-default" id="uploadmenu">
+<p>Upload your Menu</p>
+<span class="btn-file"><input type="file"></span>
+</div>
+
+<button type="submit" class="btn btn-success" name="submit" input type="submit"><span class="glyphicon glyphicon-send" aria-hidden="true"></span> Submit</button>
+</form>
